e6ef3c9ace84274fdae9e7394559eb662c30b7e6
Rathan Kumar Chikkam <38204857+rathan8825@users.noreply.github.com>
2021-10-28 11:03:10 +0200

    users and user items done

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Navbar from './components/layout/Navbar';
+import Users from './components/users/Users';
 import './App.css';
 
 class App extends Component {
@@ -9,6 +10,9 @@ class App extends Component {
     return (
       <div className='App'>
         <Navbar title='GitHub-Finder' />
+        <div className='container'>
+          <Users />
+        </div>
       </div>
     );
   }
