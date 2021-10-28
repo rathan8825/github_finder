49ca83449b165edac17e5a499807956fc6b8ff85
Rathan Kumar Chikkam <38204857+rathan8825@users.noreply.github.com>
2021-10-28 12:36:24 +0200

    spinner component and github api client id and secret

--- src/App.js
+++ src/App.js
@@ -1,17 +1,33 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
 import './App.css';
 
 class App extends Component {
-  method_1 = () => 'this is from method_1';
+  //initial state declaration
+  state = {
+    users: [], //empty users list
+    loading: false,
+  };
+  async componentDidMount() {
+    //set loading to true
+    this.setState({ loading: true });
+    //make a request
+    const res = await axios.get(
+      `https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    );
+    //set state
+    this.setState({ users: res.data, loading: false });
+  }
 
   render() {
     return (
       <div className='App'>
         <Navbar title='GitHub-Finder' />
         <div className='container'>
-          <Users />
+          {/* pass the user props onto the users component */}
+          <Users loading={this.state.loading} users={this.state.users} />
         </div>
       </div>
     );
--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -1,40 +1,29 @@
-import React, { Component } from 'react';
+import React from 'react';
 import UserItem from './UserItem';
+import Spinner from '../layout/Spinner';
+import PropTypes from 'prop-types';
 
-class Users extends Component {
-  state = {
-    users: [
-      {
-        id: '1',
-        login: 'mojombo',
-        avatar_url: 'https://avatars.githubusercontent.com/u/1?v=4',
-        html_url: 'https://github.com/mojombo',
-      },
-      {
-        id: '2',
-        login: 'defunkt',
-        avatar_url: 'https://avatars.githubusercontent.com/u/2?v=4',
-        html_url: 'https://github.com/defunkt',
-      },
-      {
-        id: '3',
-        login: 'pjhyett',
-        avatar_url: 'https://avatars.githubusercontent.com/u/3?v=4',
-        html_url: 'https://github.com/pjhyett',
-      },
-    ],
-  };
-  render() {
+const Users = ({ users, loading }) => {
+  //here we can write Users = (props) => {}...
+  //but de-structuring will help to use the props directly and can be written as
+  //const Users = ({ users, loading }) => {
+  if (loading) {
+    return <Spinner />;
+  } else {
     return (
       <div style={userStyle}>
-        {this.state.users.map((user) => (
+        {/*map through the users array and send user to useritem as props*/}
+        {users.map((user) => (
           <UserItem key={user.id} user={user} />
         ))}
       </div>
     );
   }
-}
-
+};
+Users.propTypes = {
+  users: PropTypes.array.isRequired,
+  loading: PropTypes.bool.isRequired,
+};
 const userStyle = {
   display: 'grid',
   gridTemplateColumns: 'repeat(3, 1fr)',
