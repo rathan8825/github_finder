5cf9a6f301461f34c7cd8f70d071f9515b7cb294
Rathan Kumar Chikkam <38204857+rathan8825@users.noreply.github.com>
2021-10-30 16:10:21 +0200

    single user data route complete

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -1,9 +1,17 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
+import Spinner from '../layout/Spinner';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 export class User extends Component {
   componentDidMount() {
     this.props.getUser(this.props.match.params.login);
   }
+  static propTypes = {
+    loading: PropTypes.bool.isRequired,
+    user: PropTypes.object.isRequired,
+    getUser: PropTypes.func.isRequired,
+  };
   render() {
     const {
       name,
@@ -20,10 +28,48 @@ export class User extends Component {
       hireable,
     } = this.props.user;
     const { loading } = this.props;
+    if (loading) return <Spinner />;
     return (
-      <div>
-        <p>{name}</p>
-      </div>
+      <Fragment>
+        <Link to='/' className='btn btn-light'>
+          Back to search
+        </Link>
+        hireable:{' '}
+        {hireable ? (
+          <i className='fas fa-check text-success' />
+        ) : (
+          <i className='fas fa-times-circle text-danger' />
+        )}
+        <div className='card grid-2'>
+          <div className='all-center'>
+            <img
+              src={avatar_url}
+              className='round-img'
+              alt=''
+              style={{ width: '150px' }}
+            />
+            <h1>{name}</h1>
+            <p>Location: {location}</p>
+          </div>
+          <div>
+            {bio && (
+              <Fragment>
+                <h3>Bio</h3>
+                <p>{bio}</p>
+              </Fragment>
+            )}
+            <a href={html_url} className='btn btn-dark my-1'>
+              Go to Github profile
+            </a>
+          </div>
+        </div>
+        <div className='card text-center'>
+          <div className='badge badge-primary'>Followers: {followers}</div>
+          <div className='badge badge-success'>Following: {following}</div>
+          <div className='badge badge-light'>Public Repos: {pubilc_repos}</div>
+          <div className='badge badge-dark'>Public Gists: {public_gists}</div>
+        </div>
+      </Fragment>
     );
   }
 }
